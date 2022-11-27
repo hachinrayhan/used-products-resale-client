6c4f0260e5b4932503b951e689214a0f1f8518c1
hachinrayhan <rayhan.hachin@gmail.com>
2022-11-27 20:59:15 +0600

    footer added

--- src/pages/shared/Footer.js
+++ src/pages/shared/Footer.js
@@ -2,9 +2,33 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer className="p-10 bg-center">
+            <div className='footer lg:place-items-center'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a href='/' className="link link-hover">Branding</a>
+                    <a href='/' className="link link-hover">Design</a>
+                    <a href='/' className="link link-hover">Marketing</a>
+                    <a href='/' className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a href='/' className="link link-hover">About us</a>
+                    <a href='/' className="link link-hover">Contact</a>
+                    <a href='/' className="link link-hover">Jobs</a>
+                    <a href='/' className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a href='/' className="link link-hover">Terms of use</a>
+                    <a href='/' className="link link-hover">Privacy policy</a>
+                    <a href='/' className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <div className='mt-8'>
+                <p>Copyright © 2023 - All right reserved by Buy&Sell-UsedPhones</p>
+            </div>
+        </footer>
     );
 };
 
