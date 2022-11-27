2077a7babd03201f8fdd0cbee1d0b0e19780c984
hachinrayhan <rayhan.hachin@gmail.com>
2022-11-27 20:32:43 +0600

    layout and router created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './routes/Routes';
+
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App max-w-[95%] lg:max-w-screen-xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster />
     </div>
   );
 }
